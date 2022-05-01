b40025eaf14a3150764cae2af7342431c9f0dcb3
Tcadle434 <thomascadle434@gmail.com>
2022-05-01 16:23:56 -0600

    trying load image

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import SoldOutComponent from "../../components/SoldOutComponent/SoldOutComponent";
 import styles from "./HomePage.module.scss";
+import BackgroundPhoto from "../../assets/background.png"
 
 const HomePage = () => {
+    const [isImageLoaded, setIsImageLoaded] = React.useState(false);
+
+    useEffect(() => {
+        const image = new Image();
+        image.onload = () => setIsImageLoaded(true);
+        image.src = BackgroundPhoto;
+    
+        return () => {
+          image.onload = null;
+        };
+    }, []);
+    
+    if (!isImageLoaded) {
+        return null;
+    }
+
+    console.log(isImageLoaded);
+
     return(
-    <div className={styles.page}>
+    <div className={styles.page} style={{backgroundImage: `url(${BackgroundPhoto})`}}>
         <section className={styles.page__filter}>
             <Navbar />
             <div className={styles.page__text}>
