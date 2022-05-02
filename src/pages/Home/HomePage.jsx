094c94cdd249c0620197066dc2760ff3d6170fc2
Tcadle434 <thomascadle434@gmail.com>
2022-05-01 18:17:39 -0600

    eh

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -8,31 +8,32 @@ import useProgressiveImage from "../../hooks/useProgressiveImage";
 import TestPhoto from "../../assets/PizzaLogo.png"
 
 const HomePage = (props) => {
-    const [isImageLoaded, setIsImageLoaded] = useState(false);
+    // const [isImageLoaded, setIsImageLoaded] = useState(false);
 
-    useEffect(() => {
-        console.log(`useEffect: ${isImageLoaded}`);
-        const image = new Image();
-        image.onload = () => setIsImageLoaded(true);
-        image.src = BackgroundPhoto;
+    // useEffect(() => {
+    //     console.log(`useEffect: ${isImageLoaded}`);
+    //     const image = new Image();
+    //     image.onload = () => setIsImageLoaded(true);
+    //     image.src = BackgroundPhoto;
     
-        return () => {
-          image.onload = null;
-        };
-    }, []);
+    //     return () => {
+    //       image.onload = null;
+    //     };
+    // }, []);
     
-    if (!isImageLoaded) {
-        return null;
-    }
+    // if (!isImageLoaded) {
+    //     return null;
+    // }
 
-    console.log(isImageLoaded);
-    props.onLoad(isImageLoaded);
+    // console.log(isImageLoaded);
+    // props.onLoad(isImageLoaded);
+
+
     // const loaded = useProgressiveImage(source)
 
 
     return(
-    <React.Fragment>
-    {isImageLoaded && <div className={styles.page} style={{ backgroundImage: `url(${BackgroundPhoto})` }}>
+    <div className={styles.page} style={{ backgroundImage: `url(${BackgroundPhoto})` }}>
         <section className={styles.page__filter}>
             <Navbar />
             <div className={styles.page__text}>
@@ -40,8 +41,7 @@ const HomePage = (props) => {
             </div>
             <SoldOutComponent />
         </section>
-    </div> }
-    </React.Fragment>
+    </div> 
     );
 }
 
