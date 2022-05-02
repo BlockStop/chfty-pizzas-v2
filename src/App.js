c1404b17fd14f1ffa4a93bc01378874e2970a1d6
Tcadle434 <thomascadle434@gmail.com>
2022-05-01 21:13:59 -0600

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import BackgroundPhoto from "./assets/background.png"
 import './App.css';
 
 const HomePage = React.lazy(() => import('./pages/Home/HomePage'));
-
+const Home = React.lazy(() => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(import("./pages/Home/HomePage")), 1000);
+  });
+});
 function App(props) {
   // const [imgLoad, setImgLoaded] = useState(false);
 
@@ -20,7 +24,7 @@ function App(props) {
         <Routes>
           {/* <Route path='/' element={<HomePage source={BackgroundPhoto} placeholder={<CustomLoader />}/>} /> */}
             {/* <Route path='/' element={<HomePage onLoad={onLoadHandler} />} /> */}
-            <Route path='/' element={<HomePage />} />
+            <Route path='/' element={<Home />} />
             <Route path='/gallery' element={<HomePage />} />
             <Route path='/about' element={<HomePage />} />
             <Route path='/menu' element={<HomePage />} />
